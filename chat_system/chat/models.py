fd12775daed692196027bc4af12e1b78ef0b0839
Phu Myat Thwe <52267857+pmyatthwe@users.noreply.github.com>
2024-06-05 10:09:33 +0630

    Update models.py

--- chat_system/chat/models.py
+++ chat_system/chat/models.py
@@ -7,23 +7,38 @@ class ChatRoom(models.Model):
     name = models.CharField(max_length=32)
     created_at = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return self.name
+
 
 class Message(models.Model):
     class MessageType(models.TextChoices):
         TEXT = 'TXT', _('Text')
         IMAGE = 'IMG', _('Image')
 
-    sender_id = models.ForeignKey(User, on_delete=models.SET_NULL)
-    chat_room_id = models.ForeignKey(ChatRoom, on_delete=models.CASCADE)
+    sender = models.ForeignKey(User, on_delete=models.SET_NULL, related_name='sender_messages')
+    chat_room = models.ForeignKey(ChatRoom, on_delete=models.CASCADE, related_name='chat_room_messages')
     content = models.CharField(max_length=255, null=True, blank=True)
     message_type = models.CharField(max_length=3, choices=MessageType.choices, default=MessageType.TEXT)
     timestamp = models.DateTimeField(auto_now_add=True)
     seen = models.BooleanField(default=False)
 
+    class Meta:
+        ordering = ['timestamp']
+
+    def __str__(self):
+        return f"{self.message_type}"
+
 
 class UserChatRoom(models.Model):
-    chat_room_id = models.ForeignKey(ChatRoom, on_delete=models.CASCADE)
-    user_id = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+    chat_room = models.ForeignKey(ChatRoom, on_delete=models.CASCADE, related_name='chat_room')
+    user = models.ForeignKey(User, null=True, on_delete=models.SET_NULL, related_name='user_chat_room')
+
+    class Meta:
+        unique_together = ('chat_room', 'user')
+
+    def __str__(self):
+        return f"{self.user} in {self.chat_room}"
 
 
 #https://bytebytego.com/courses/system-design-interview/design-a-chat-system
